16eb7f1a520169066c4fcd2e6f57ce5d6598b505
Sven Skoglund <svenrskoglund@gmail.com>
2021-08-07 22:47:08 -0600

    Set up univers template

--- flask/app.py
+++ flask/app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template("./index.html", name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+    return render_template("./index.html")
 
-@app.route("/about")
+@app.route("/works.html")
+def works():
+    return render_template("./works.html")
+
+@app.route("/about.html")
 def about():
     return render_template("./about.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("./contact.html")
+
+@app.route("/components.html")
+def components():
+    return render_template("./components.html")
 
 @app.route("/blog/2020/dogs")
 def blog2():
