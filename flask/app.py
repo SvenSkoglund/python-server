eea8238505f51387028a4707e11e7287a6057266
Sven Skoglund <svenrskoglund@gmail.com>
2021-08-08 00:27:28 -0600

    Set up basic contact form function

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -9,3 +9,12 @@ def page_route(page):
 @app.route("/")
 def my_home():
     return render_template("./index.html")
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "Something went wrong"
