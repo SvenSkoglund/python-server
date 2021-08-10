13f2218335e891d4835ced4535a21b497d31a7f1
Sven Skoglund <svenrskoglund@gmail.com>
2021-08-10 12:18:07 -0600

    Update to write to csv, create requirements.txt

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -13,9 +14,12 @@ def my_home():
 @app.route('/submit_form', methods=['GET', 'POST'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect("/thankyou.html")
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+        except:
+            return 'Something went wrong saving to db'
     else:
         return "Something went wrong"
 
@@ -26,3 +30,11 @@ def write_to_file(data):
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
